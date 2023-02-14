9d6803d239e269a3edeb34b960d8df13fdb60f43
waleriQ <interceptor55@icloud.com>
2023-02-15 00:07:40 +0600

    slice

---
+++ script.js
@@ -0,0 +1,10 @@
+//****slice*****
+//arr.slice([start], [end])
+// let arr = ["t", "e", "s", "t"];
+// let brr = arr.slice(0,1);
+// console.log(brr);
+
+// CREATE COPY
+let arr = ["t", "e", "s", "t"];
+let brr = arr.slice();
+console.log(brr);
